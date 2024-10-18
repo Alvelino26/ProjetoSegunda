84d313841ed93204bb13ec0ab6326f36ed329d64
unknown <leonadoa93@gmail.com.br>
2024-10-18 20:24:42 -0300

    acho que agora sim

--- Projeto/Projeto/Models/Adocao.cs
+++
@@ -1,9 +0,0 @@
-public class Adocao
-{
-    public int Id { get; set; }
-    public int AnimalId { get; set; }
-    public Animal? Animal { get; set; }
-    public int AdotanteId { get; set; }
-    public Adotante? Adotante { get; set; }
-    public string? Status { get; set; } // Pendente, Concluída, Cancelada
-}
