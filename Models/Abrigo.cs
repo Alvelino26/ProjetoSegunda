9db91d07ac65d18b1151af9778b5cd948ae1edcd
unknown <leonadoa93@gmail.com.br>
2024-10-18 19:46:42 -0300

    commit rambo

---
+++ Models/Abrigo.cs
@@ -0,0 +1,10 @@
+public class Abrigo
+{
+    public int Id { get; set; }
+    public string? Nome { get; set; }
+    public string? Endereco { get; set; }
+    public string? Telefone { get; set; }
+    public ICollection<Animal>? Animais { get; set; }
+}
+
+//teste rambo
