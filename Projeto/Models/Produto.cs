513defc84070660b401d95d09e3df99abddf5f11
gabriel.silva <alvelinoga@gmail.com>
2024-10-17 21:57:27 -0300

    BD adicionado

---
+++ Projeto/Models/Produto.cs
@@ -0,0 +1,8 @@
+public class Produto
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public int Quantidade { get; set; }
+    public decimal Valor { get; set; }
+    public DateTime DataCriacao { get; set; }
+}
